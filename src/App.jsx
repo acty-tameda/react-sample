7a66664001150aed35e5504262b28702da32a452
tame <acty.tameda@gmail.com>
2021-07-20 23:00:35 +0900

    No.46 in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert("ボタンが押されました。");
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <h1 style={{ color: "red", margin: "16px" }}>Hello World!</h1>
       <ColorfulMessage color="red" message="I'm Tomohiro!" />
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
